f720dde7cb5da19cdc0bdd34322b5352e6bac4e0
parth-vakil <66581951+parth-vakil@users.noreply.github.com>
2020-06-21 21:04:54 -0700

    Attempt to use heroku database

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 
 var pool = new Pool({
-  connectionString: 'postgres://postgres:cmpt276@localhost/person'
+  connectionString: process.env.DATABASE_URL
 });
 
 express()
